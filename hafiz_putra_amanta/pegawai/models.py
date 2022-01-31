8a08dbbaf5bb13feb8a8f0707b6197adf40340f5
hafizpanst <hafizpanst@gmail.com>
2022-01-31 16:10:02 +0700

    update

--- hafiz_putra_amanta/pegawai/models.py
+++ hafiz_putra_amanta/pegawai/models.py
@@ -1,9 +1,14 @@
+from random import choice
 from django.db import models
 
 # Create your models here.
-class Pegawai (models.Model):
+class Pegawai(models.Model):
     nip = models.CharField(max_length=18, primary_key=True)
     name = models.CharField(max_length=100)
     bidang = models.CharField(max_length=50)
     seksi = models.CharField(max_length=50)
     jabatan = models.CharField(max_length=50)
+
+class Users(models.Model):
+    username = models.ForeignKey(to=Pegawai, on_delete=models.SET_NULL, null=True)
+    role = models.CharField(max_length=50)
