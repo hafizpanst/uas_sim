bb26b94b7bda4a1f45bc0d7ef15ca9b99132f28d
hafizpanst <hafizpanst@gmail.com>
2022-02-01 00:43:38 +0700

    update

--- hafiz_putra_amanta/settings.py
+++ hafiz_putra_amanta/settings.py
@@ -148,7 +148,7 @@ if DEBUG:
         os.path.join(BASE_DIR, "static"),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
